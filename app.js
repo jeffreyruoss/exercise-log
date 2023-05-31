3b004d50829c7dd69720d41ff80e433ca512e409
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-05-31 15:19:02 -0700

    save to local storage

--- app.js
+++ app.js
@@ -15,52 +15,62 @@ class Exercise {
   }
 }
 
-const exercises = [
-  'handstands',
-  'hike',
-  'trail run',
-  'street run',
-  'Hang',
-  'Pullups underhand',
-  'Pullups overhand',
-  'Rows bands',
-  'Rows gym straight',
-  'Rows gym wide',
-  'Squats',
-  'Pushups straight',
-  'Pushups center hand',
-  'Pushups inclined',
-  'bench press',
-  'Planks full, half, side',
-  'Dr K back (on side and on laying on back anchored)',
-  'triceps kettle bed',
-  'triceps dumbbells gym',
-  'triceps skull crushers',
-  'triceps pull down bands pole',
-  'triceps pull down gym rope',
-  'triceps pull down gym bar',
-  'Crunches (3 variations)',
-  'Biceps dumbbells',
-  'Biceps kettle',
-  'Forearms kettle',
-  'Forearms dumbbells',
-  'dumbbell laying on my side, to the side, shoulder exercise',
-  'shoulder kettle',
-  'shoulders dumbbells',
-  'shoulder broom',
-  'Chest press (laundry pole)',
-  'Isometric Internal and External Rotation - elbow by side and push against hand',
-  'back band',
-  'ankle star',
-  'calf raises - straight and at angles (inward and outward) - with weight',
-  'leg extensions (on the chair, legs extending forward)',
-  'Leg extensions upward with bands on high chair outside',
-  'leg extensions side (Pull band apart with legs)',
-  'forefoot balance on one leg',
-  'Squats',
-  'ankles side to side outward',
-  'ankles side to side inward'
-].map((name) => new Exercise(name));
+let exercises = localStorage.getItem('exercises');
+
+if (exercises) {
+  exercises = JSON.parse(exercises).map((exercise) => {
+    const ex = new Exercise(exercise.name);
+    ex.timestamp = new Date(exercise.timestamp);
+    return ex;
+  });
+} else {
+  exercises = [
+    'handstands',
+    'hike',
+    'trail run',
+    'street run',
+    'Hang',
+    'Pullups underhand',
+    'Pullups overhand',
+    'Rows bands',
+    'Rows gym straight',
+    'Rows gym wide',
+    'Squats',
+    'Pushups straight',
+    'Pushups center hand',
+    'Pushups inclined',
+    'bench press',
+    'Planks full, half, side',
+    'Dr K back (on side and on laying on back anchored)',
+    'triceps kettle bed',
+    'triceps dumbbells gym',
+    'triceps skull crushers',
+    'triceps pull down bands pole',
+    'triceps pull down gym rope',
+    'triceps pull down gym bar',
+    'Crunches (3 variations)',
+    'Biceps dumbbells',
+    'Biceps kettle',
+    'Forearms kettle',
+    'Forearms dumbbells',
+    'dumbbell laying on my side, to the side, shoulder exercise',
+    'shoulder kettle',
+    'shoulders dumbbells',
+    'shoulder broom',
+    'Chest press (laundry pole)',
+    'Isometric Internal and External Rotation - elbow by side and push against hand',
+    'back band',
+    'ankle star',
+    'calf raises - straight and at angles (inward and outward) - with weight',
+    'leg extensions (on the chair, legs extending forward)',
+    'Leg extensions upward with bands on high chair outside',
+    'leg extensions side (Pull band apart with legs)',
+    'forefoot balance on one leg',
+    'Squats',
+    'ankles side to side outward',
+    'ankles side to side inward'
+  ].map((name) => new Exercise(name));
+}
 
 function render() {
   const list = document.getElementById('exercise-list');
@@ -82,6 +92,7 @@ window.markAsDone = (name) => {
   const exercise = exercises.find((ex) => ex.name === name);
   if (exercise) {
     exercise.updateTimestamp();
+    localStorage.setItem('exercises', JSON.stringify(exercises));
     render();
   }
 };
