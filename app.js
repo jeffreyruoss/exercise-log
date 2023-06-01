9785f1cc3f62dc5ac8567e4c5a1714631615ec4a
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-06-01 14:45:14 -0700

    Clean up.

--- app.js
+++ app.js
@@ -26,49 +26,9 @@ if (exercises) {
   });
 } else {
   exercises = [
-    'handstands',
-    'hike',
-    'trail run',
-    'street run',
-    'Hang',
-    'Pullups underhand',
-    'Pullups overhand',
-    'Rows bands',
-    'Rows gym straight',
-    'Rows gym wide',
-    'Pushups straight',
-    'Pushups center hand',
-    'Pushups inclined',
-    'bench press',
-    'Planks full, half, side',
-    'Dr K back (on side and on laying on back anchored)',
-    'triceps kettle bed',
-    'triceps dumbbells gym',
-    'triceps skull crushers',
-    'triceps pull down bands pole',
-    'triceps pull down gym rope',
-    'triceps pull down gym bar',
-    'Crunches (3 variations)',
-    'Biceps dumbbells',
-    'Biceps kettle',
-    'Forearms kettle',
-    'Forearms dumbbells',
-    'dumbbell laying on my side, to the side, shoulder exercise',
-    'shoulder kettle',
-    'shoulders dumbbells',
-    'shoulder broom',
-    'Chest press (laundry pole)',
-    'Isometric Internal and External Rotation - elbow by side and push against hand',
-    'back band',
-    'ankle star',
-    'calf raises - straight and at angles (inward and outward) - with weight',
-    'leg extensions (on the chair, legs extending forward)',
-    'Leg extensions upward with bands on high chair outside',
-    'leg extensions side (Pull band apart with legs)',
-    'forefoot balance on one leg',
-    'Squats',
-    'ankles side to side outward',
-    'ankles side to side inward'
+    'exercise 1',
+    'exercise 2',
+    'exercise 3'
   ].map((name) => new Exercise(name));
 }
 
@@ -90,8 +50,8 @@ function render() {
     `;
 
     if (exercise.paused) {
-      li.classList.add('paused'); // Add the 'paused' class
-      pausedList.appendChild(li); // Add to the paused list
+      li.classList.add('paused');
+      pausedList.appendChild(li);
     } else {
       list.appendChild(li);
     }
