a947c453284710d0e82e0166736fcc20ae823e01
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-07-21 17:45:14 -0700

    Removing Firebase from gitignore.

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD3y-57uze139gYvq8uagnKt9F7IY34jUM",
+  authDomain: "exercise-log-8a9b4.firebaseapp.com",
+  projectId: "exercise-log-8a9b4",
+  storageBucket: "exercise-log-8a9b4.appspot.com",
+  messagingSenderId: "323872244593",
+  appId: "1:323872244593:web:8faa7dbb2c60dab7132cd0"
+};
