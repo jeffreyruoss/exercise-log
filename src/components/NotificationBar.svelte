b897ce82ede32bd500223a6b176f688138ebb9cf
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-08-01 13:53:30 -0700

    Fix notification fade.

--- src/components/NotificationBar.svelte
+++ src/components/NotificationBar.svelte
@@ -3,46 +3,33 @@
   import { fade } from 'svelte/transition';
   let notification = { message: '', type: '' };
   
-  // subscribe to changes in the store
   notificationStore.subscribe(value => {
     notification = value;
   });
-  
-  function showNotification(message, type = 'success') {
-    notificationStore.set({ message, type });
-    setTimeout(() => {
-      notificationStore.set({ message: '', type: '' });
-    }, 3000);
-  }
 </script>
 
-<div
-	class="notification-bar {notification.type} {notification.message ? 'show' : ''}" 
-	in:fade={{duration: 300}} 
-	out:fade={{duration: 300}}
->
-  {notification.message}
-</div>
+{#if notification.message}
+  <div
+    class="notification-bar {notification.type}" 
+    in:fade={{duration: 200}} 
+    out:fade={{duration: 200}}
+  >
+    {notification.message}
+  </div>
+{/if}
 
 <style>
-	.notification-bar {
-		display: none;
+  .notification-bar {
 		position: fixed;
 		top: 0;
 		left: 0;
 		width: 100%;
-		padding: 5px;
-		background-color: #2095C1;
-		text-align: center;
-		color: #fff;
-	}
-	.notification-bar.show {
-		display: block;
-	}
-  .success {
-    /* add your styles here */
+    padding: 5px;
+    background-color: #2095C1;
+    text-align: center;
+    color: #fff;
   }
-  .show {
+  .success {
     /* add your styles here */
   }
 </style>
